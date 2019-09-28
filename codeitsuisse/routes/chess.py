e321a713c2b0fa2c320dbb52718aa6c25901ef90
Nick Chin <44158435+hermitP@users.noreply.github.com>
2019-09-28 11:53:19 +0800

    chs

--- codeitsuisse/routes/chess.py
+++ codeitsuisse/routes/chess.py
@@ -10,7 +10,6 @@ logger = logging.getLogger(__name__)
 @app.route('/chessgame', methods=['POST'])
 def chess():
     board = request.get_json()
-    logging.info("data sent for evaluation {}".format(board))
 
     nrow = len(board)
     ncol = len(board[0])
@@ -18,8 +17,10 @@ def chess():
 
     for i in range(nrow):
         for j in range(ncol):
-            if "K" in board[i][j]:
+            if 'K' in board[i][j]:
                 queen = [i, j]
+
+    logging.info("queen position {}".format(queen))
 
     def direction(i: int, j: int):
         nonlocal squares 
@@ -28,9 +29,9 @@ def chess():
         while True:
             stepRow = stepRow + i
             stepCol = stepCol + j
-            if stepRow >= 0 and stepRow < nrow and stepCol >= 0 and stepCol < ncol:
+            if stepRow < 0 and stepRow >= nrow and stepCol < 0 and stepCol >= ncol :
                 break
-            if board[stepRow][stepCol] == "X":
+            if board[stepRow][stepCol] == 'X':
                 break
             squares += 1
     
