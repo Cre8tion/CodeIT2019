ea96659e3e0b25315bdbea07f8a2132012853796
Cre8tion <ljl1997@gmail.com>
2019-09-28 12:50:22 +0800

    test

--- codeitsuisse/routes/jar.py
+++ codeitsuisse/routes/jar.py
@@ -11,10 +11,35 @@ logger = logging.getLogger(__name__)
 def ready():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    print(data)
-    result = ""
-    #inputValue = data.get("input");
-    #result = inputValue * inputValue
+    num = data.get("maxChoosableInteger")
+    total = data.get("desiredTotal")
+    if(num == 1):
+    	count = -1
+    else:
+	    lst = list(range(1,num+1))
+	    limit = num + 1
+	    turn = 0
+	    if(num >= total/2 and num < total):
+	        count = 3
+	    elif(num > total):
+	        count = 1
+	    else:
+		    while(len(lst) > 0 and total - lst[-1] > limit):
+		    	turn+=1
+		    	total = total - lst[-1]
+		    	lst.pop(-1)
+		    	limit = lst[-1]
+		    	print(turn)
+		    	print(total)
+		    	print(lst)
+		    if (len(lst) == 0 and total > 0):
+		    	count = -1
+		    else:
+		    	count = turn + 2
+    if(count % 2 == 0):
+        count = -1
+    result = {"res":count}
+
     logging.info("My result :{}".format(result))
     return json.dumps(result);
 
