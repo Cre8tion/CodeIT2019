8140b202bbd3dffb6ed3f966d754af6f10955908
Cre8tion <ljl1997@gmail.com>
2019-09-28 10:52:23 +0800

    logging command

--- codeitsuisse/routes/jar.py
+++ codeitsuisse/routes/jar.py
@@ -11,8 +11,10 @@ logger = logging.getLogger(__name__)
 def evaluate():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input");
-    result = inputValue * inputValue
+    print(data)
+    result = ""
+    #inputValue = data.get("input");
+    #result = inputValue * inputValue
     logging.info("My result :{}".format(result))
     return json.dumps(result);
 
