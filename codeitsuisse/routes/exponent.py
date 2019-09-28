8f536a54e4ad90812eb7691e000f3cc00e8928bf
Cre8tion <ljl1997@gmail.com>
2019-09-28 20:21:43 +0800

    Fix Bug

--- codeitsuisse/routes/exponent.py
+++ codeitsuisse/routes/exponent.py
@@ -10,34 +10,39 @@ def exponent():
     n = data['n']
     p = data['p']
 
-    length = p * math.log10(n)
-    fd = int(str(10 ** length)[0])
-    length = int(math.ceil(length))
+    if(n == 0):
+        fd = 0
+        length = 1
+        ldigit = 0
+    else:
+        length = p * math.log10(n)
+        fd = int(str(10 ** length)[0])
+        length = int(math.ceil(length))
 
-    num = str(n)
+        num = str(n)
 
-    #
-    #exp = str(n ** p)
-    #logging.info("val " + exp)
-    #
-    #fd = int(exp[0])
-    #length = len(str(exp))
-    
-    #lst = [int(x) for x in str(num)] 
+        #
+        #exp = str(n ** p)
+        #logging.info("val " + exp)
+        #
+        #fd = int(exp[0])
+        #length = len(str(exp))
+        
+        #lst = [int(x) for x in str(num)] 
 
-    #mod = Modulo(4, lst)
+        #mod = Modulo(4, lst)
 
-    #if((mod == 0)) : 
-    #    exp_end = 4
-    #else :  
-    #    exp_end = mod
-    
-    # Find last digit in 'a' and compute its exponent 
-    #print(num)
-    res = int(num[len(num)-1])**p
-    ldigit = int(res % 10) 
+        #if((mod == 0)) : 
+        #    exp_end = 4
+        #else :  
+        #    exp_end = mod
+        
+        # Find last digit in 'a' and compute its exponent 
+        #print(num)
+        res = int(num[len(num)-1])**p
+        ldigit = int(res % 10) 
 
-    #ld = int(exp[-1])
+        #ld = int(exp[-1])
 
     lst = [fd, length, ldigit]
     result = {"result" : lst}
