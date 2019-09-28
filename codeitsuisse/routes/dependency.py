69bc24ed1526bd2c79fb0f735577caad9520a12e
Nick Chin <44158435+hermitP@users.noreply.github.com>
2019-09-28 12:15:28 +0800

    test dependency

--- codeitsuisse/routes/dependency.py
+++ codeitsuisse/routes/dependency.py
@@ -1,19 +1,31 @@
 import logging
 import json
 
-from flask import request, jsonify;
+from flask import request, jsonify
 
-from codeitsuisse import app;
+from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
 @app.route('/generateSequence', methods=['POST'])
 def seq():
-    data = request.get_json();
+    data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    print(data)
-    result = ""
-    #inputValue = data.get("input");
-    #result = inputValue * inputValue
-    logging.info("My result :{}".format(result))
-    return json.dumps(result);
+    
+
+    nodes = data["modules"]
+    edges = {}
+
+    for d in data["dependencyPairs"]:
+        start = d['dependentOn']
+        end = d['dependee']
+        if start in edges:
+            edges[start] = edges[start].append(end)
+        else:
+            edges[start] = [end]
+
+    logging.info("nodes {}".format(nodes))
+    logging.info("edges {}".format(edges))
+    
+
+    return json.dumps([])
